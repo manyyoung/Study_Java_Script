095fb226c865aa91b7aa944fbd936212021589f9
manyyoung <manyyoung.limit@gmail.com>
2022-01-09 21:31:23 +0900

    [수정] 52 문

--- js100/javascript_100.js
+++ js100/javascript_100.js
@@ -833,3 +833,32 @@ function mergeSort(arr){
   const array = [4, 2, 3, 8, 5];
   
   console.log(mergeSort(array));
+
+// 52. quick sort
+{
+    function quickSort(arr){
+        if (arr.length <= 1){
+          return arr;
+        }
+      
+        const pivot = arr[0];
+        const left = [];
+        const right = [];
+      
+        for (let i=1; i<arr.length; i++){
+          /*빈칸을 채워주세요*/
+          if(arr[i] < pivot){
+            left.push(arr[i]);
+          } else {
+            right.push(arr[i]);
+          }
+        }
+
+        /*빈칸을 채워주세요*/
+        return quickSort(left).concat(pivot, quickSort(right));
+      }
+      
+    const array = [4, 2, 3, 8, 5];
+      
+    console.log(quickSort(array));
+}
