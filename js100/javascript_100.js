ffc776318c0baa60edef3b9bb84453db2d0ad443
manyyoung <manyyoung.limit@gmail.com>
2022-01-21 22:32:46 +0900

    [수정] 60 문

--- js100/javascript_100.js
+++ js100/javascript_100.js
@@ -1194,3 +1194,21 @@ function mergeSort(arr){
     //padStart(길이, 채울 문자열) : 주어진 길이만큼 원래 문자열의 왼쪽부터 주어진 문자열로 채움
     //padEnd(길이, 채울 문자열) : 주어진 길이만큼 원래 문자열의 오른쪽부터 주어진 문자열로 채움
 }
+
+// 60. 번호 매기기 새 학기가 되어 이름을 가나다 순서대로 배정하고 번호를 매기려고 합니다. 데이터에 입력된 이름을 아래와 같이 출력해 주세요.
+{
+    const students = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리','한지호','김진이','김민호','강채연'];
+
+    let studentsSorting = students.sort();
+
+    const result = [];
+
+    for(let i = 0 ; i < studentsSorting.length ; i++) {
+        result.push({
+            '번호': i + 1,
+            '이름': studentsSorting[i]
+        });
+    }
+    
+    console.log(result);
+}
