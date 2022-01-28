816e29a91ef148974c48f2124b9dc17ea52bab20
manyyoung <manyyoung.limit@gmail.com>
2022-01-28 15:51:39 +0900

    [수정] 64 문 답

--- js100/javascript_100.js
+++ js100/javascript_100.js
@@ -1294,5 +1294,21 @@ function mergeSort(arr){
 {
     let inputValue = 24;
 
-    
+    // 답안
+    let N = inputValue;
+    let result = 0;
+
+    while (true){
+    if (N%7 === 0){
+        result += parseInt(N/7, 10);
+        console.log(result);
+        break;
+    }
+    N -= 3;
+    result += 1;
+        if (N < 0){
+            console.log(-1);
+            break;
+        }
+    }
 }
