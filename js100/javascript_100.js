c75d08886d797be408bdb3636668caa73d4e5e03
manyyoung <manyyoung.limit@gmail.com>
2022-02-12 22:06:34 +0900

    [수정] 65 문

--- js100/javascript_100.js
+++ js100/javascript_100.js
@@ -1312,3 +1312,49 @@ function mergeSort(arr){
         }
     }
 }
+
+// 65. a = [1, 2, 3, 4], b = [a, b, c, d] 이런 리스트가 있을 때 [[1, a], [b, 2], [3, c], [d, 4]] 이런 식으로 a, b 리스트가 번갈아가면서 출력되게 해주세요.
+{
+    let a = [1, 2, 3, 4]; 
+    let b = ['a', 'b', 'c', 'd'];
+
+    let result = [];
+    
+    for (let i = 0 ; i < a.length ; i++) {
+        let tempArr = [];
+        if (i % 2 === 0) {
+            tempArr = [a[i], b[i]];
+        } else {
+            tempArr = [b[i], a[i]];
+        }
+
+        result.push(tempArr);
+    }
+
+    console.log(result);
+
+    // 답안
+    // 방법 1 - forEach 사용
+    let c = [];
+
+    a.forEach(function(e, i){
+    if (i%2 === 0){
+        c.push([e, b[i]]);
+    } else {
+        c.push([b[i], e]);
+    }
+    });
+
+    console.log(c);
+
+    // 방법 2 - map 사용
+    let d = a.map(function(e, i){
+        if(i%2 === 0){
+            return [e, b[i]];
+        } else{
+            return [b[i], e];
+        }
+    });
+
+console.log(d);
+}
